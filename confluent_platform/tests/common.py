a71f27cd3fade8a131a3eaa24df66d2c762b7149
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-03-02 18:12:27 +0100

    draft

--- confluent_platform/tests/common.py
+++ confluent_platform/tests/common.py
@@ -35,3 +35,35 @@ ACTIVEMQ_E2E_METRICS = [
     "activemq.broker.temp_pct",
     "activemq.broker.memory_pct",
 ]
+
+
+TYPE_GAUGE = 'com.yammer.metrics.reporting.JmxReporter$Gauge'
+
+
+class Attribute(object):
+    def __init__(self, name, check_metric=True):
+        self.name = name
+        self.check_metric = check_metric
+
+
+class MBean(object):
+    def __init__(self, bean_name, clazz, attrs=None, check_metric=True):
+        self.bean_name = bean_name
+        self.clazz = clazz
+        self.attrs = attrs
+        self.check_metric = check_metric
+        self.domain = None
+
+    @staticmethod
+    def _parse_name(name):
+
+
+
+
+MBEANS = [
+    MBean('kafka.server:type=ReplicaManager,name=UnderMinIsrPartitionCount', TYPE_GAUGE),
+    MBean('kafka.server:type=ReplicaManager,name=UnderReplicatedPartitions', TYPE_GAUGE),
+    # MBean('kafka.cluster:type=Partition,topic={topic},name=UnderMinIsr,partition={partition}', attrs=[
+    #     Attribute('hello')
+    # ]),
+]
